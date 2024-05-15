4ca942cb786cda978c9db58af8bbe257365f4aff
david01prof <david01prof@hotmail.com>
2024-05-15 19:47:02 +0200

    feature-inputSignal

--- src/app/src/components/products/card/card.component.ts
+++ src/app/src/components/products/card/card.component.ts
@@ -1,8 +1,24 @@
-import { Component, Input, numberAttribute } from '@angular/core';
+import { Component, Input, OnChanges, computed, input, numberAttribute } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
-function addDiscountProperty(product:any){
+function addDiscountProperty(product:Product):Product{
   return {discount: false, ...product};
+}
+
+interface Rating{
+  rate: number;
+  count: number;
+}
+
+interface Product {
+  id: number;
+  title: string;
+  price:number;
+  description: string;
+  category: string;
+  image: string;
+  rating: Rating;
+  discount?:boolean;
 }
 
 @Component({
@@ -12,8 +28,28 @@ function addDiscountProperty(product:any){
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
-export class CardComponent {
+export class CardComponent{
 
-    @Input({required: true, transform: addDiscountProperty}) product !: any;
+    // Esto esta en fase beta
+    
+    product = input.required({transform: addDiscountProperty})
+
+    rating = computed(() =>{
+        const { rating } = this.product();
+        return Object.values(rating);
+    });
+
+    /* @Input({required: true, transform: addDiscountProperty}) product !: any;
     // Transform permite modificar la data q me llega
+
+    rating : string[] = [];
+
+    ngOnChanges(changes:any):void{
+
+      if(changes.product){
+        const { rating } = this.product;
+        this.rating = Object.values(rating);
+      }
+    }*/
+    
 }
