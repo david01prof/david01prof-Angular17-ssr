61b52257378a5b5a7b9d6f302623cb12e84a8c7b
david01prof <david01prof@hotmail.com>
2024-05-17 12:50:10 +0200

    feature-signalState

--- src/app/src/components/counter/counter.component.ts
+++ src/app/src/components/counter/counter.component.ts
@@ -1,4 +1,5 @@
 import { Component, computed, effect, signal } from '@angular/core';
+import { patchState, signalState } from '@ngrx/signals';
 
 @Component({
   selector: 'app-counter',
@@ -8,23 +9,45 @@ import { Component, computed, effect, signal } from '@angular/core';
   styleUrl: './counter.component.scss'
 })
 export class CounterComponent {
-  counter = signal<number>(0);
 
-  double = computed(() => this.counter() * 2);
+  // RXJS normal con signal
 
+  // counter = signal<number>(0);
+
+  // double = computed(() => this.counter() * 2);
+
+  // constructor(){
+  //   effect(()=> console.log('this value is:' + this.counter()))
+  // }
+
+  // increment(){
+  //   this.counter.update((current:number)=> current + 1)
+  // }
+
+  // decrement(){
+  //   this.counter.update((current:number)=> current - 1)
+  // }
+
+  // reset(){
+  //   this.counter.set(0);
+  // }
+
+  // RXJS con signalState
+
+  state = signalState({counter: 0});
   constructor(){
-    effect(()=> console.log('this value is:' + this.counter()))
+    effect(()=> console.log('this value is:' + this.state.counter()))
   }
 
   increment(){
-    this.counter.update((current:number)=> current + 1)
+    patchState(this.state,(state)=>({counter: state.counter + 1 }))
   }
 
   decrement(){
-    this.counter.update((current:number)=> current - 1)
+    patchState(this.state,(state)=>({counter: state.counter - 1 }))
   }
 
   reset(){
-    this.counter.set(0);
+    patchState(this.state,()=>({counter: 0 }))
   }
 }
