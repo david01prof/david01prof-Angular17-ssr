0a5cc530b286eaa68606bde133621ca37d4cede4
david01prof <david01prof@hotmail.com>
2024-05-17 18:11:50 +0200

    feature-whithMethod_Hooks

--- src/app/store/cart.store.ts
+++ src/app/store/cart.store.ts
@@ -1,18 +1,23 @@
-import { patchState, signalStore, withMethods, withState } from "@ngrx/signals";
+import { patchState, signalStore, withComputed, withHooks, withMethods, withState } from "@ngrx/signals";
 import { Product } from "../src/components/products/card/card.component";
+import { computed } from "@angular/core";
 
 export interface CartState {
     products: Product[];
+    count:number;
 }
 
 const initialState: CartState = {
     products:[],
+    count: 0
 };
 
 export const CartStore = signalStore(
     {providedIn:'root'},
     withState(initialState),
-
+    withComputed(({products})=>({
+        count: computed(()=> products().length)
+    })),
     withMethods(({ products, ...store }) => ({
         addToCart( product:Product ):void{
             const  updatedProducts = [ ...products(), product ];
@@ -22,5 +27,10 @@ export const CartStore = signalStore(
             const updatedProduct = products().filter(product => product.id  != id);
             patchState(store,{ products: updatedProduct})
         }
-    })) 
+    })) ,
+
+    withHooks({
+        onInit: (store)=> console.log('Store init', store),
+        onDestroy: (store)=> console.log('Store Destroy',store)                
+    })
 )
